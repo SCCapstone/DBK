5c86a9835e74d90f6e20893815cda4a69fd79d21
hxtk <peter@psanders.me>
2019-02-12 18:48:27 -0500

    Add queries to BoostBoxDao

--- Android/app/src/main/java/edu/sc/dbkdrymatic/internal/database/BoostBoxDao.java
+++ Android/app/src/main/java/edu/sc/dbkdrymatic/internal/database/BoostBoxDao.java
@@ -1,4 +1,29 @@
 package edu.sc.dbkdrymatic.internal.database;
 
-public class BoostBoxDao {
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+import edu.sc.dbkdrymatic.internal.BoostBox;
+
+@Dao
+public interface BoostBoxDao {
+  @Query("SELECT * FROM BoostBox")
+  List<BoostBox> getAll();
+
+  @Query("SELECT * FROM BoostBox WHERE address LIKE :address LIMIT 1")
+  BoostBox getByAddress(String address);
+
+  @Insert
+  void insertAll(BoostBox... boxes);
+
+  @Delete
+  void delete(BoostBox boostBox);
+
+  @Update
+  void update(BoostBox boostBox);
 }
