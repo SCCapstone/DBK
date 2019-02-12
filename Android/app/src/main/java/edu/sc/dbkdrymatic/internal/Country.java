33550c2a3d421d6ac8ece96b7f62e75b639e5648
hxtk <peter@psanders.me>
2019-02-12 18:45:41 -0500

    Add javadoc

--- Android/app/src/main/java/edu/sc/dbkdrymatic/internal/Country.java
+++ Android/app/src/main/java/edu/sc/dbkdrymatic/internal/Country.java
@@ -1,5 +1,13 @@
 package edu.sc.dbkdrymatic.internal;
 
+/**
+ * Enumeration of the different countries available for selection.
+ *
+ * <p>The different countries have different kilowatt ratings based on the voltage and frequency
+ * of electricity delivery in those countries at the wall. These are national standards that have
+ * been stored here as constant values and associated with the name of their country so that this
+ * selection may be done transparently to the user.
+ */
 public enum Country {
   USA ("United States", 1.4),
   UK  ("United Kingdom", 2.05),
--- Android/app/src/main/java/edu/sc/dbkdrymatic/internal/database/AppDatabase.java
+++ Android/app/src/main/java/edu/sc/dbkdrymatic/internal/database/AppDatabase.java
@@ -1,9 +1,28 @@
-package edu.sc.dbkdrymatic.internal;
+package edu.sc.dbkdrymatic.internal.database;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {SiteInfo.class}, version = 1)
+import edu.sc.dbkdrymatic.internal.BoostBox;
+import edu.sc.dbkdrymatic.internal.SiteInfo;
+
+/**
+ * Room Persistence Database manager for DBK Drymatic app.
+ *
+ * <p>This is the provider through which DAO objects should be instantiated, e.g.,
+ * {@code SiteInfoDao}, {@code BoostBoxDao}, {@code JobDao}, and the method through which those
+ * objects should be instantiated. Those objects are abstraction layers over this class, and cannot
+ * exist on their own.
+ *
+ * <p>This class should be instantiated using a {@code RoomDatabase.Builder} generated using
+ * {@code android.arch.persistence.room.Room.databaseBuilder()}. For documentation, see
+ * {@code https://developer.android.com/reference/android/arch/persistence/room/Room.html}.
+ */
+@Database(entities = {SiteInfo.class, BoostBox.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+  /** Returns an abstraction for storage and retrieval of {@code SiteInfo} objects */
   public abstract SiteInfoDao siteInfoDao();
+
+  /** Returns an abstraction for storage and retrieval of {@code BoostBox} objects.*/
+  public abstract BoostBoxDao boostBoxDao();
 }
