ccfcde40c3d575c53f5dbffe0732dea6df34a50b
hxtk <peter@psanders.me>
2019-02-12 18:57:29 -0500

    Add typeconverters needed by boostbox

--- Android/app/src/main/java/edu/sc/dbkdrymatic/internal/database/Converters.java
+++ Android/app/src/main/java/edu/sc/dbkdrymatic/internal/database/Converters.java
@@ -1,14 +1,64 @@
-package edu.sc.dbkdrymatic.internal;
+package edu.sc.dbkdrymatic.internal.database;
 
 import android.arch.persistence.room.TypeConverter;
 
 import org.jscience.physics.amount.Amount;
 
+import javax.measure.quantity.ElectricCurrent;
+import javax.measure.quantity.ElectricPotential;
+import javax.measure.quantity.Energy;
+import javax.measure.quantity.Power;
 import javax.measure.quantity.Temperature;
 import javax.measure.quantity.Volume;
 import javax.measure.unit.SI;
 
+import edu.sc.dbkdrymatic.internal.Country;
+import edu.sc.dbkdrymatic.internal.Damage;
+
+/**
+ * Collection of converters between the unit-independent types used by this application and
+ * serializable types suitable for storage in the Database.
+ */
 public class Converters {
+  @TypeConverter
+  public static double voltageToSI(Amount<ElectricPotential> value) {
+    return value.doubleValue(SI.VOLT);
+  }
+
+  public static Amount<ElectricPotential> voltageFromSI(double value) {
+    return Amount.valueOf(value, SI.VOLT);
+  }
+
+  @TypeConverter
+  public static double currentToSI(Amount<ElectricCurrent> value) {
+    return value.doubleValue(SI.AMPERE);
+  }
+
+  @TypeConverter
+  public static Amount<ElectricCurrent> currentFromSi(double value) {
+    return Amount.valueOf(value, SI.AMPERE);
+  }
+
+  @TypeConverter
+  public static double powerToSi(Amount<Power> value) {
+    return value.doubleValue(SI.WATT);
+  }
+
+  @TypeConverter
+  public static Amount<Power> powerFromSI(double value) {
+    return Amount.valueOf(value, SI.WATT);
+  }
+
+  @TypeConverter
+  public static double energyToSi(Amount<Energy> value) {
+    return value.doubleValue(SI.JOULE);
+  }
+
+  @TypeConverter
+  public static Amount<Energy> enegyFromSi(double value) {
+    return Amount.valueOf(value, SI.JOULE);
+  }
+
   @TypeConverter
   public static double volumeToSI(Amount<Volume> value) {
     return value.doubleValue(SI.CUBIC_METRE);
