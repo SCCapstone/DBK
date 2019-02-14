b4df9c21c1fc3915bd5b9e2195fb22ebcbda037c
hxtk <peter@psanders.me>
2019-02-14 17:19:27 -0500

    Documentation for SiteInfo

--- Android/app/src/main/java/edu/sc/dbkdrymatic/internal/SiteInfo.java
+++ Android/app/src/main/java/edu/sc/dbkdrymatic/internal/SiteInfo.java
@@ -18,6 +18,18 @@ import javax.measure.unit.Unit;
 
 import edu.sc.dbkdrymatic.internal.database.Converters;
 
+/**
+ * Object representing the properties of a site including identifying information and measurements
+ * taken at the start of the job.
+ *
+ * This information is used to compute the values to be displayed by the calculator and some others.
+ * Namely: energy requirement, power requirement, D2 requirement, Boost Box requirement, and
+ * other requirements as needed.
+ *
+ * Two {@code SiteInfo}s are considered to be equal if and only if all of their fields are equal.
+ * In practice, one may assume two are equal if they share a primary key and were instantiated from
+ * a database query.
+ */
 @Entity
 @TypeConverters(Converters.class)
 public class SiteInfo {
@@ -69,10 +81,17 @@ public class SiteInfo {
     return this.id;
   }
 
+  /**
+   * This function is not to be used externally. The database requires this method in order to
+   * generate the primary key.
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Gives the energy requirement of the job from initial conditions, adjusted for job locale.
+   */
   public Amount<Energy> getAdjustedEnergy() {
     double energyBtu = this.getEnergy().doubleValue(BTU);
     double value = 0.0;
@@ -89,13 +108,24 @@ public class SiteInfo {
     return Amount.valueOf(value, BTU);
   }
 
+  /**
+   * Gives the theoretical energy requirement of the job, without adjusting for the difference
+   * in energy depending on configuration of the local energy grid. In practice, this will introduce
+   * losses and as such, {@code getAdjustedEnergy()} is preferred.
+   */
   public Amount<Energy> getEnergy() {
+    // Do not use the `org.jscience.amount` mathematical operations.
+    // For some reason they introduce math errors. Convert to doubles and then do math
+    // on those values.
     double desiredOutsideTempDiff = desiredTemp.doubleValue(NonSI.FAHRENHEIT);
     desiredOutsideTempDiff -= outsideTemp.doubleValue(NonSI.FAHRENHEIT);
 
     double desiredInsideTempDiff = desiredTemp.doubleValue(NonSI.FAHRENHEIT);
     desiredInsideTempDiff -= insideTemp.doubleValue(NonSI.FAHRENHEIT);
 
+    // This formula is lifted from the spreadsheet provided by DBK. Numeric coefficients
+    // have been simplified to a single value per term in the sum, but no other changes have been
+    // made.
     double energyValue =
           (volume.doubleValue(CUBIC_FOOT) * desiredOutsideTempDiff * 0.008568)
         + (volume.doubleValue(CUBIC_FOOT) * waterLoss.getValue() * 12.6)
@@ -104,23 +134,44 @@ public class SiteInfo {
     return Amount.valueOf(energyValue, BTU);
   }
 
+  /**
+   * Computes the power requirement at the outlet to complete the job in the optimal amount of time
+   * based on the characteristics of the local electrical grid.
+   */
   public Amount<Power> getAdjustedPower() {
     return Amount.valueOf(
         this.getAdjustedEnergy().doubleValue(BTU) / 3400.0, SI.KILO(SI.WATT));
   }
 
+  /**
+   * Computes the theoretical power requirement to complete the job, disregarding losses due to
+   * electrical grid configuration.
+   */
   public Amount<Power> getPower() {
     return Amount.valueOf(this.getEnergy().doubleValue(BTU) / 3400.0, SI.KILO(SI.WATT));
   }
 
+  /**
+   * Computes the number of D2 air moving units that should be installed at the job site for optimal
+   * job completion time to complement the Boost Boxes.
+   */
   public double getD2Requirement() {
     return volume.doubleValue(CUBIC_FOOT) / 6000.0;
   }
 
+  /**
+   * Computes the number of Boost Box heating units that should be installed at the job site for
+   * optimal job completion time, given the parameters of the particular site.
+   */
   public double getBoostBoxRequirement() {
     return this.getAdjustedPower().doubleValue(SI.KILO(SI.WATT)) / country.getKilowattRating();
   }
 
+  /**
+   * Compares a {@code SiteInfo} to another object. If that object is not a {@code SiteInfo}, or
+   * if the other object is a {@code SiteInfo} differing by more than 0.0001 in any floating point
+   * field or having any disagreement in discrete fields, we return false. Otherwise, return true.
+   */
   @Override
   public boolean equals(Object that) {
     if (that.getClass() != this.getClass()) return false;
