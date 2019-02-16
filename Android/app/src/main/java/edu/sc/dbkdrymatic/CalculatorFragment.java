f9ac68008914b827fbce22e0fd15d5dd7f6a989d
hxtk <peter@psanders.me>
2019-02-16 14:00:45 -0500

    Transition Calculator Fragment fully to async view model architecture

--- Android/app/src/main/java/edu/sc/dbkdrymatic/CalculatorFragment.java
+++ Android/app/src/main/java/edu/sc/dbkdrymatic/CalculatorFragment.java
@@ -1,6 +1,11 @@
 package edu.sc.dbkdrymatic;
 
-import android.app.Fragment;
+import android.arch.lifecycle.Observer;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.arch.lifecycle.ViewModelProviders;
+import android.arch.persistence.room.Room;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.Editable;
@@ -8,8 +13,6 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -20,26 +23,52 @@ import org.jscience.physics.amount.Amount;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
-import edu.sc.dbkdrymatic.internal.AppDatabase;
 import edu.sc.dbkdrymatic.internal.Damage;
+import edu.sc.dbkdrymatic.internal.Job;
 import edu.sc.dbkdrymatic.internal.Settings;
 import edu.sc.dbkdrymatic.internal.SiteInfo;
-import edu.sc.dbkdrymatic.internal.SiteInfoDao;
+import edu.sc.dbkdrymatic.internal.database.AppDatabase;
+import edu.sc.dbkdrymatic.internal.viewmodels.SelectedJobModel;
+import edu.sc.dbkdrymatic.internal.viewmodels.SettingsModel;
 
 
 public class CalculatorFragment extends Fragment {
 
-  private SiteInfo siteInfo;
   private Settings settings;
-  private SiteInfoDao dao;
+  private SelectedJobModel model;
+  private SettingsModel settingsModel;
+
+  private Job job;
   View myView;
 
-  public CalculatorFragment() {}
+  @Override
+  public void onCreate(Bundle savedState) {
+    super.onCreate(savedState);
 
-  public CalculatorFragment(SiteInfo siteInfo, Settings settings, SiteInfoDao dao) {
-    this.siteInfo = siteInfo;
-    this.settings = settings;
-    this.dao = dao;
+    // Monitor for changes to the currently selected job and update the view as appropriate
+    AppDatabase appDb = Room.databaseBuilder(
+        this.getActivity().getApplicationContext(), AppDatabase.class, "dbk.db").build();
+    SelectedJobModel.Factory sjmFactory = new SelectedJobModel.Factory(appDb.siteInfoDao());
+    this.model = ViewModelProviders.of(this.getActivity(), sjmFactory).get(SelectedJobModel.class);
+    this.model.getSelectedJob().observe(this, new Observer<Job>() {
+      @Override
+      public void onChanged(@Nullable Job job) {
+        updateView(job, settings);
+      }
+    });
+
+    // Monitor for
+    SharedPreferences preferences = PreferenceManager
+        .getDefaultSharedPreferences(this.getContext());
+    SettingsModel.Factory settingsFactory = new SettingsModel.Factory(preferences);
+    this.settingsModel = ViewModelProviders.of(this.getActivity(), settingsFactory)
+        .get(SettingsModel.class);
+    this.settingsModel.getSettings().observe(this, new Observer<Settings>() {
+      @Override
+      public void onChanged(@Nullable Settings settings) {
+        updateView(job, settings);
+      }
+    });
   }
 
   @Nullable
@@ -50,46 +79,43 @@ public class CalculatorFragment extends Fragment {
     return myView;
   }
 
-  public void setSiteInfo(SiteInfo siteInfo) {
-    this.siteInfo = siteInfo;
-  }
+  @Override
+  public void onViewCreated(View view, Bundle savedState) {
+    super.onViewCreated(view, savedState);
 
-  public void setSettings(Settings settings) {
-    this.settings = settings;
-  }
-
-  public void onViewCreated(View view, Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    // START OF CALCULATOR SCREEN
-
-    final DecimalFormat df = new DecimalFormat("#.#");
+    final Spinner damageClassSpinner = (Spinner) getView().findViewById(R.id.water_loss);
+    ArrayAdapter<Damage> adapter = new ArrayAdapter<Damage>(
+        this.getActivity(), android.R.layout.simple_spinner_item);
+    adapter.addAll(Damage.values());
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    damageClassSpinner.setAdapter(adapter);
 
     final EditText volumeField = (EditText) (getView().findViewById(R.id.volume));
-    volumeField.setText(
-        df.format(siteInfo.volume.doubleValue(settings.getVolumeUnit())));
     volumeField.addTextChangedListener(new TextWatcher() {
       @Override
-      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-      @Override
-      public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-        double value = 0.0;
-        try {
-          value = Double.parseDouble(volumeField.getText().toString());
-        } catch (NumberFormatException e) {
-          value = 0.0;
-        }
-        siteInfo.volume = Amount.valueOf(value, settings.getVolumeUnit());
-        dao.update(siteInfo);
       }
 
       @Override
-      public void afterTextChanged(Editable editable) {}
+      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        double value;
+        try {
+          value = Double.parseDouble(charSequence.toString());
+        } catch (NumberFormatException e) {
+          value = 0;
+        }
+        job.getSiteInfo().volume = Amount.valueOf(value, settings.getVolumeUnit());
+        model.update(job);
+      }
+
+      @Override
+      public void afterTextChanged(Editable editable) {
+
+      }
     });
 
     final EditText insideTempField = (EditText) (getView().findViewById(R.id.inside_temp));
-    insideTempField.setText(
-        df.format(siteInfo.insideTemp.doubleValue(settings.getTemperatureUnit())));
     insideTempField.addTextChangedListener(new TextWatcher() {
       @Override
       public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -98,14 +124,14 @@ public class CalculatorFragment extends Fragment {
 
       @Override
       public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        double value = 0.0;
+        double value;
         try {
-          value = Double.parseDouble(insideTempField.getText().toString());
+          value = Double.parseDouble(charSequence.toString());
         } catch (NumberFormatException e) {
-          value = 0.0;
+          value = 0;
         }
-        siteInfo.insideTemp = Amount.valueOf(value, settings.getTemperatureUnit());
-        dao.update(siteInfo);
+        job.getSiteInfo().insideTemp = Amount.valueOf(value, settings.getTemperatureUnit());
+        model.update(job);
       }
 
       @Override
@@ -115,9 +141,7 @@ public class CalculatorFragment extends Fragment {
     });
 
     final EditText desiredTempField = (EditText) (getView().findViewById(R.id.desired_temp));
-    desiredTempField.setText(
-        df.format(siteInfo.desiredTemp.doubleValue(settings.getTemperatureUnit())));
-    desiredTempField.addTextChangedListener(new TextWatcher() {
+    insideTempField.addTextChangedListener(new TextWatcher() {
       @Override
       public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -125,14 +149,14 @@ public class CalculatorFragment extends Fragment {
 
       @Override
       public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        double value = 0.0;
+        double value;
         try {
-          value = Double.parseDouble(desiredTempField.getText().toString());
+          value = Double.parseDouble(charSequence.toString());
         } catch (NumberFormatException e) {
-          value = 0.0;
+          value = 0;
         }
-        siteInfo.desiredTemp = Amount.valueOf(value, settings.getTemperatureUnit());
-        dao.update(siteInfo);
+        job.getSiteInfo().desiredTemp = Amount.valueOf(value, settings.getTemperatureUnit());
+        model.update(job);
       }
 
       @Override
@@ -142,9 +166,7 @@ public class CalculatorFragment extends Fragment {
     });
 
     final EditText outsideTempField = (EditText) (getView().findViewById(R.id.outside_temp));
-    outsideTempField.setText(
-        df.format(siteInfo.outsideTemp.doubleValue(settings.getTemperatureUnit())));
-    outsideTempField.addTextChangedListener(new TextWatcher() {
+    insideTempField.addTextChangedListener(new TextWatcher() {
       @Override
       public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -152,14 +174,14 @@ public class CalculatorFragment extends Fragment {
 
       @Override
       public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        double value = 0.0;
+        double value;
         try {
-          value = Double.parseDouble(outsideTempField.getText().toString());
+          value = Double.parseDouble(charSequence.toString());
         } catch (NumberFormatException e) {
-          value = 0.0;
+          value = 0;
         }
-        siteInfo.outsideTemp = Amount.valueOf(value, settings.getTemperatureUnit());
-        dao.update(siteInfo);
+        job.getSiteInfo().outsideTemp = Amount.valueOf(value, settings.getTemperatureUnit());
+        model.update(job);
       }
 
       @Override
@@ -170,8 +192,7 @@ public class CalculatorFragment extends Fragment {
 
     final EditText relativeHumidityField =
         (EditText) (getView().findViewById(R.id.relative_humidity));
-    relativeHumidityField.setText(df.format(siteInfo.relativeHumidity));
-    relativeHumidityField.addTextChangedListener(new TextWatcher() {
+    insideTempField.addTextChangedListener(new TextWatcher() {
       @Override
       public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -179,14 +200,14 @@ public class CalculatorFragment extends Fragment {
 
       @Override
       public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        double value = 0.0;
+        double value;
         try {
-          value = Double.parseDouble(relativeHumidityField.getText().toString());
+          value = Double.parseDouble(charSequence.toString());
         } catch (NumberFormatException e) {
-          value = 0.0;
+          value = 0;
         }
-        siteInfo.relativeHumidity = value;
-        dao.update(siteInfo);
+        job.getSiteInfo().relativeHumidity = value;
+        model.update(job);
       }
 
       @Override
@@ -195,39 +216,41 @@ public class CalculatorFragment extends Fragment {
       }
     });
 
+  }
+
+  public void updateView(Job job, Settings settings) {
+    this.job = job;
+    this.settings = settings;
+
+    final DecimalFormat df = new DecimalFormat("#.#");
+    final SiteInfo siteInfo = job.getSiteInfo();
+
+    final EditText volumeField = (EditText) (getView().findViewById(R.id.volume));
+    volumeField.setText(
+        df.format(siteInfo.volume.doubleValue(settings.getVolumeUnit())));
+    final EditText insideTempField = (EditText) (getView().findViewById(R.id.inside_temp));
+    insideTempField.setText(
+        df.format(siteInfo.insideTemp.doubleValue(settings.getTemperatureUnit())));
+
+    final EditText desiredTempField = (EditText) (getView().findViewById(R.id.desired_temp));
+    desiredTempField.setText(
+        df.format(siteInfo.desiredTemp.doubleValue(settings.getTemperatureUnit())));
+
+    final EditText outsideTempField = (EditText) (getView().findViewById(R.id.outside_temp));
+    outsideTempField.setText(
+        df.format(siteInfo.outsideTemp.doubleValue(settings.getTemperatureUnit())));
+
+    final EditText relativeHumidityField =
+        (EditText) (getView().findViewById(R.id.relative_humidity));
+    relativeHumidityField.setText(df.format(siteInfo.relativeHumidity));
+
     final Spinner damageClassSpinner = (Spinner) getView().findViewById(R.id.water_loss);
-
-    ArrayAdapter<Damage> adapter = new ArrayAdapter<Damage>(this.getActivity(), android.R.layout.simple_spinner_item);
-    adapter.addAll(Damage.values());
-    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    damageClassSpinner.setAdapter(adapter);
-
     damageClassSpinner.setSelection(Arrays.asList(Damage.values()).indexOf(siteInfo.waterLoss));
-    damageClassSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-      @Override
-      public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        siteInfo.waterLoss = (Damage) damageClassSpinner.getSelectedItem();
-        dao.update(siteInfo);
-      }
 
-      @Override
-      public void onNothingSelected(AdapterView<?> adapterView) {
+    final TextView boostBoxes = (TextView) getView().findViewById(R.id.boost_boxes);
+    boostBoxes.setText(df.format(siteInfo.getBoostBoxRequirement()));
 
-      }
-    });
-
-    Button calculate = (Button) getView().findViewById(R.id.calculate);
-    calculate.setOnClickListener(new View.OnClickListener() {//when user clicks calculate button
-      @Override
-      public void onClick(View view) {
-        TextView boost_boxes = (TextView)  getView().findViewById(R.id.boost_boxes);
-        TextView d2s = (TextView)  getView().findViewById(R.id.d2s);
-
-        boost_boxes.setText(df.format(siteInfo.getBoostBoxRequirement()));
-        d2s.setText(df.format(siteInfo.getD2Requirement()));
-      }
-
-    });
-    //END OF CALCULATOR SCREEN
+    final TextView d2s = (TextView) getView().findViewById(R.id.d2s);
+    d2s.setText(df.format(siteInfo.getD2Requirement()));
   }
 }
--- Android/app/src/main/java/edu/sc/dbkdrymatic/internal/viewmodels/SelectedJobModel.java
+++ Android/app/src/main/java/edu/sc/dbkdrymatic/internal/viewmodels/SelectedJobModel.java
@@ -8,8 +8,6 @@ import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.util.List;
-
 import edu.sc.dbkdrymatic.internal.Job;
 import edu.sc.dbkdrymatic.internal.database.SiteInfoDao;
 
@@ -24,11 +22,40 @@ public class SelectedJobModel extends ViewModel implements Observer<Job> {
     this.siDao = siDao;
   }
 
+  /**
+   * Change the selected {@code Job} monitored by {@code getSelectedJob}.
+   */
   public void setSelectedJob(Job job) {
+    if (selectedJob == null) {
+      selectedJob = new MutableLiveData<>();
+    }
+
+    // Stop observing the old "current job"
+    this.siDao.getJob(this.selectedJobId).removeObserver(this);
+
+    // Update and begin observing the new current job.
     this.selectedJobId = job.getSiteInfo().getId();
     this.siDao.getJob(this.selectedJobId).observeForever(this);
   }
 
+  /**
+   * Update the currently selected job in the database.
+   *
+   * Note, this operation is only valid if the job passed in is the currently selected job. This
+   * is enforced by the primary key found at {@code Job.getSiteInfo().getId()}. The program will
+   * crash with an assertion failure if this is not respected.
+   */
+  public void update(Job job) {
+    assert job.getSiteInfo().getId() == selectedJob.getValue().getSiteInfo().getId();
+    this.siDao.update(job.getSiteInfo());
+  }
+
+  /**
+   * Returns a {@code LiveData<Job>} which will always be up-to-date as long as it exists.
+   *
+   * <p>The returned object will send updates to observers if the selected {@code Job} is updated in
+   * the database or if a different {@code Job} is selected.
+   */
   public LiveData<Job> getSelectedJob() {
     return this.selectedJob;
   }
