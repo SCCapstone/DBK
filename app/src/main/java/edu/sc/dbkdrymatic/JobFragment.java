eab57d68eddf2e8e10c13d4998582c03a6248c33
hxtk <peter@psanders.me>
2019-03-03 19:05:03 -0500

    Closes #60

--- app/src/main/java/edu/sc/dbkdrymatic/JobFragment.java
+++ app/src/main/java/edu/sc/dbkdrymatic/JobFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -20,16 +22,34 @@ public class JobFragment extends Fragment {
       LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
     this.view = inflater.inflate(R.layout.job_layout, container, false);
 
-    ViewPager pager = view.findViewById(R.id.job_pager);
+    return this.view;
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle state) {
+    super.onViewCreated(view, state);
+
+    ViewPager pager = this.view.findViewById(R.id.job_pager);
     pager.setAdapter(new TabAdapter(getFragmentManager()));
 
-    return this.view;
+    TabLayout tabs = this.view.findViewById(R.id.job_tabs);
+    tabs.setupWithViewPager(pager);
+
   }
 
   private class TabAdapter extends FragmentPagerAdapter {
 
     public TabAdapter(FragmentManager manager) {
       super(manager);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      if (position == 0) {
+        return JobFragment.this.getString(R.string.calculator_tab);
+      } else {
+        return JobFragment.this.getString(R.string.bluetooth_tab);
+      }
     }
 
     @Override
