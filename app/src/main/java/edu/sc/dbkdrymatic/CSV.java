4fc5a4b2fd95f9db3465020a0c997ed0f2596f70
GHBrown <ghbrown@email.sc.edu>
2019-04-29 22:18:02 -0400

    changed IO scheme for csv writer

--- app/src/main/java/edu/sc/dbkdrymatic/CSV.java
+++ app/src/main/java/edu/sc/dbkdrymatic/CSV.java
@@ -24,33 +24,37 @@ import java.util.List;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
+import javax.swing.text.BoxView;
+
+import edu.sc.dbkdrymatic.internal.BoostBox;
+
 public class CSV {
 
-/**public static void writeDataLine(String Filepath) //todo add filepath then delete later
-{
- File ToSend = new File(Filepath );
+   /** public void writeDataLine(String Filepath) //todo add filepath then delete later
+    {
+        File ToSend = new File(Filepath);
 
-         try{
+        try {
             FileWriter outputfile = new FileWriter(ToSend); //Todo fix ToSend needs to be written to a file to be emailed
 
             CSVWriter writer = new CSVWriter(outputfile);
 
-            List<String[]> data = new ArrayList<String []>();
-            data.add(new String[]{"VALUE","DATA"});//for header
-            data.add(new String[]{"ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-            data.add(new String[]{"ARBname","ARBdata"});//
-         }
-}**/
+            List<String[]> data = new ArrayList<String[]>();
+            data.add(new String[]{"VALUE", "DATA"});//for header
+            for (BoostBox boostBox; List < boostBox >) {
+                CSVWriter.
+                (getClass(boostBox.getAddress(), boostBox.getAirInTemp(), boostBox.getAirOutTarget(),
+                        boostBox.getAirOutTemp(), boostBox.getAirOutThreshold(), boostBox.getCumulativeEnergy()
+                        , boostBox.getCurrent(), boostBox.getHours(), boostBox.getJobId(), boostBox.getPower(), boostBox.isAutoRestart(),
+                        boostBox.getName(), boostBox.isRunning(), boostBox.getVoltage());
+            }
+            writer.writeAll(data);
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    **/
 }
 
